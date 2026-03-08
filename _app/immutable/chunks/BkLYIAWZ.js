ec913cdc74fbe5fadb11da1d148ad308c185e6a0
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-03-07 23:21:12 -0500

    Add tour as landing page for new users

--- _app/immutable/chunks/BkLYIAWZ.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_m385cs?.base??"/grammar-cards-game",a=globalThis.__sveltekit_m385cs?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/NbfFcqsb.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1f0hq7s?.base??"/grammar-cards-game",a=globalThis.__sveltekit_1f0hq7s?.assets??s??"";export{a,s as b};
