69580b8ada310072a858fa29beb4ebb5ba88c3f9
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-03-07 23:40:26 -0500

    Deploy script fix

---
+++ _app/immutable/chunks/CPRGWTi2.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1gc0jq3?.base??"/grammar-cards-game",a=globalThis.__sveltekit_1gc0jq3?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/NbfFcqsb.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1f0hq7s?.base??"/grammar-cards-game",a=globalThis.__sveltekit_1f0hq7s?.assets??s??"";export{a,s as b};
