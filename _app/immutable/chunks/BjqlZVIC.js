d72fe5b09768b4f2bf2bfa13de5d26540384f914
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-03-07 21:49:26 -0500

    ${1:-Deploy}

---
+++ _app/immutable/chunks/BjqlZVIC.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_10ch81c?.base??"/grammar-cards-game",a=globalThis.__sveltekit_10ch81c?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/C32QNfHx.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_2k1cuo?.base??"/grammar-cards-game",a=globalThis.__sveltekit_2k1cuo?.assets??s??"";export{a,s as b};
