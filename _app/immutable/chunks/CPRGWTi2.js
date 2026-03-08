afb6dcb549458322326653014c40d45044724add
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-03-08 10:41:36 -0500

    Better tour page

--- _app/immutable/chunks/CPRGWTi2.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1gc0jq3?.base??"/grammar-cards-game",a=globalThis.__sveltekit_1gc0jq3?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/D9a-G_fl.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_137qdhu?.base??"/grammar-cards-game",a=globalThis.__sveltekit_137qdhu?.assets??s??"";export{a,s as b};
