2d04b6be8429b18d3040e13e59fa35ba53741703
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-03-08 10:46:49 -0500

    Fix a11y warning in Dedication component

--- _app/immutable/chunks/D9a-G_fl.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_137qdhu?.base??"/grammar-cards-game",a=globalThis.__sveltekit_137qdhu?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/DYbsgw_Y.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_61tfi0?.base??"/grammar-cards-game",a=globalThis.__sveltekit_61tfi0?.assets??s??"";export{a,s as b};
