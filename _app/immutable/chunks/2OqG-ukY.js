be564ab18db31583683c9f0ef8f8e056d9d73a2a
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-03-09 10:55:22 -0500

    Fix 87 Spanish gerund-vs-infinitive exercises: disfrutar + gerundio → infinitivo

--- _app/immutable/chunks/2OqG-ukY.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1qoad1w?.base??"/grammar-cards-game",a=globalThis.__sveltekit_1qoad1w?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/DViRFmBd.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_9hfscw?.base??"/grammar-cards-game",a=globalThis.__sveltekit_9hfscw?.assets??s??"";export{a,s as b};
