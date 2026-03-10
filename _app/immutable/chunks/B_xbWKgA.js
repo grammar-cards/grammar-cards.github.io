4af659f777bcd910762ec25bebdfb0763cfb0979
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-03-10 10:04:03 -0500

    Increase coffee-note font size on ApiKeySetup to match other screens

--- _app/immutable/chunks/B_xbWKgA.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_mwtz14?.base??"",t=globalThis.__sveltekit_mwtz14?.assets??s??"";export{t as a,s as b};
---
+++ _app/immutable/chunks/C8HxVwDT.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_v00j3n?.base??"",a=globalThis.__sveltekit_v00j3n?.assets??s??"";export{a,s as b};
