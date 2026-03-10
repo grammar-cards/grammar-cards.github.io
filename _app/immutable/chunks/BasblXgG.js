71fc1f237c9886b8d352a31c38e6dba155fd40c7
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-03-10 08:56:08 -0500

    Fix base path for root domain deploy - fixes Safari loading and PWA blank screen

--- _app/immutable/chunks/BasblXgG.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_sbvcwk?.base??"/grammar-cards-game",a=globalThis.__sveltekit_sbvcwk?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/plSQzPA6.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_d1b1dr?.base??"",a=globalThis.__sveltekit_d1b1dr?.assets??s??"";export{a,s as b};
