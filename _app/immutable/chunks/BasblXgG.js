d2aca68dc1fb1765178f21df3399e9f7d13f9140
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-03-10 08:19:41 -0500

    Fix tense tag matching for wish/if only and other colon-separated tense names

---
+++ _app/immutable/chunks/BasblXgG.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_sbvcwk?.base??"/grammar-cards-game",a=globalThis.__sveltekit_sbvcwk?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/BdeEdP5L.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1m4l6xf?.base??"/grammar-cards-game",a=globalThis.__sveltekit_1m4l6xf?.assets??s??"";export{a,s as b};
