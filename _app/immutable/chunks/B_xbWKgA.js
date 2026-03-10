064ff311121aa4503f45df42a525127571f623bc
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-03-10 09:43:25 -0500

    Update API key explanation text - friendlier tone, mention cost we cover

---
+++ _app/immutable/chunks/B_xbWKgA.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_mwtz14?.base??"",t=globalThis.__sveltekit_mwtz14?.assets??s??"";export{t as a,s as b};
--- _app/immutable/chunks/plSQzPA6.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_d1b1dr?.base??"",a=globalThis.__sveltekit_d1b1dr?.assets??s??"";export{a,s as b};
