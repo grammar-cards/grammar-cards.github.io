698294bbaa7c0039d986cf2544d21b18b3702dcc
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-03-11 23:21:37 -0500

    Fix drag and drop reorder across multiple rows in sentence area

--- _app/immutable/chunks/DzONmE67.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_eqdkhe?.base??"",e=globalThis.__sveltekit_eqdkhe?.assets??s??"";export{e as a,s as b};
---
+++ _app/immutable/chunks/Kz3Yny4a.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_2obo8r?.base??"",a=globalThis.__sveltekit_2obo8r?.assets??s??"";export{a,s as b};
