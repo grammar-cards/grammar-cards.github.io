4d2618cb2018cd486f8c858d5ffba931011181bc
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-03-11 22:31:30 -0500

    Tour screenshots: better exercises, error feedback, populated stats; SEO fixes; /tour route; favicon PNG fallback; a11y fix

---
+++ _app/immutable/chunks/DzONmE67.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_eqdkhe?.base??"",e=globalThis.__sveltekit_eqdkhe?.assets??s??"";export{e as a,s as b};
--- _app/immutable/chunks/Vs3jIRX6.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1imcam7?.base??"",a=globalThis.__sveltekit_1imcam7?.assets??s??"";export{a,s as b};
