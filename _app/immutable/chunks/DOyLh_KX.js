cfcf38eb8134dde15e6b51febda2c9a93bfb7546
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-03-25 00:36:22 -0500

    Fix beginner exercises: present-continuous (18), present-simple (28), nature topic pools

---
+++ _app/immutable/chunks/DOyLh_KX.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_15s84v1?.base??"",a=globalThis.__sveltekit_15s84v1?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/DPYSnzXL.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_77qguw?.base??"",a=globalThis.__sveltekit_77qguw?.assets??s??"";export{a,s as b};
