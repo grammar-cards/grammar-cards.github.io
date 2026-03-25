11a9e8290b386d83b4eda620e6b179579b010c3e
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-03-25 15:12:38 -0500

    Fix beginner there-is-are exercises and add slug support to scripts

---
+++ _app/immutable/chunks/D8yMjxi-.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_8lf4xm?.base??"",a=globalThis.__sveltekit_8lf4xm?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/DOyLh_KX.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_15s84v1?.base??"",a=globalThis.__sveltekit_15s84v1?.assets??s??"";export{a,s as b};
