b9ebccba18648fcaf0ce577f7110b96ba1d7aa57
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-03-28 00:36:37 -0500

    Semantic fixes, translation cleanup, and stale key removal across all exercises

---
+++ _app/immutable/chunks/CpBH2-bK.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_pxcpjl?.base??"",a=globalThis.__sveltekit_pxcpjl?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/cqJCXgLn.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_4qjw2j?.base??"",a=globalThis.__sveltekit_4qjw2j?.assets??s??"";export{a,s as b};
