d2715c74b286ae3ea06bccaab4d513dc50d5668a
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-03-28 20:23:40 -0500

    Missing translations

---
+++ _app/immutable/chunks/DwdXCexo.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_u6dudv?.base??"",a=globalThis.__sveltekit_u6dudv?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/FgqVYU1T.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1mnok2l?.base??"",a=globalThis.__sveltekit_1mnok2l?.assets??s??"";export{a,s as b};
