87e3f9b12ccf1aedbb0719cd1d6451520bc26f4e
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-03-29 22:14:58 -0500

    Fix iOS audio: kill orphan elements, debounce toggles

--- _app/immutable/chunks/DAkXL8XU.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_bxw3yo?.base??"",a=globalThis.__sveltekit_bxw3yo?.assets??s??"";export{a,s as b};
--- _app/immutable/entry/start.sAbolkFc.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/CSa5Qob-.js";export{o as load_css,r as start};
