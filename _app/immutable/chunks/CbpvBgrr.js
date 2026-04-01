5fb0143295b743a138f8643d7c4cb24d77dfc82d
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-03-31 23:04:27 -0500

    Pistas improvements

---
+++ _app/immutable/chunks/CbpvBgrr.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1gezuj8?.base??"",e=globalThis.__sveltekit_1gezuj8?.assets??s??"";export{e as a,s as b};
--- _app/immutable/chunks/DMwKrhN6.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_7ul9i4?.base??"",a=globalThis.__sveltekit_7ul9i4?.assets??s??"";export{a,s as b};
