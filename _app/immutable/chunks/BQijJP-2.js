e43016f5a6a21d43413ff064c87683f183081894
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-04-02 03:56:54 -0500

    Hints improvements completed

---
+++ _app/immutable/chunks/BQijJP-2.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_afxocw?.base??"",a=globalThis.__sveltekit_afxocw?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/CbpvBgrr.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1gezuj8?.base??"",e=globalThis.__sveltekit_1gezuj8?.assets??s??"";export{e as a,s as b};
