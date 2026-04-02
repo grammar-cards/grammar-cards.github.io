097a736914fff82bee35e10fc6c9f09879823688
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-04-02 05:24:43 -0500

    Avoid repeating completed exercises

--- _app/immutable/chunks/BQijJP-2.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_afxocw?.base??"",a=globalThis.__sveltekit_afxocw?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/Coibf-o5.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_zbawuz?.base??"",a=globalThis.__sveltekit_zbawuz?.assets??s??"";export{a,s as b};
