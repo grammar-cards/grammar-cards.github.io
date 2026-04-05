8aa0a0cd12359926f5e2cd96f5b8aafffe61c725
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-04-05 14:46:44 -0500

    Add translation note to help panel

---
+++ _app/immutable/chunks/CBAIS04C.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1rh043i?.base??"",a=globalThis.__sveltekit_1rh043i?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/Coibf-o5.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_zbawuz?.base??"",a=globalThis.__sveltekit_zbawuz?.assets??s??"";export{a,s as b};
