f521cfc16513b3250ee6bb5ba19e30b3c338ce30
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-04-06 09:20:16 -0500

    Improve all_completed panel: retry failed exercises directly, fix repeat bug

---
+++ _app/immutable/chunks/CcWJMYdw.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_pc9v0b?.base??"",a=globalThis.__sveltekit_pc9v0b?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/DG9RR2XR.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_otvlas?.base??"",a=globalThis.__sveltekit_otvlas?.assets??s??"";export{a,s as b};
