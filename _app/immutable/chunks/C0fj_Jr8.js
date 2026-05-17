2c6a92d887a7d2c1821ae37ffe76acacc7aad658
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-05-16 19:25:21 -0500

    Remove API key dialog from onboarding and profile selector

--- _app/immutable/chunks/C0fj_Jr8.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1apg8r5?.base??"",a=globalThis.__sveltekit_1apg8r5?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/C8NBpzpO.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_h16y9r?.base??"",a=globalThis.__sveltekit_h16y9r?.assets??s??"";export{a,s as b};
