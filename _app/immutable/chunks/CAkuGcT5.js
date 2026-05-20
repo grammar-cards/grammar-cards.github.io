4a5e5d8da6624c021646ee1f10d8443cbf1ca7ec
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-05-20 05:17:56 -0500

    Better toggle control

--- _app/immutable/chunks/CAkuGcT5.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1d9e7ti?.base??"",e=globalThis.__sveltekit_1d9e7ti?.assets??s??"";export{e as a,s as b};
---
+++ _app/immutable/chunks/WZv8B2zc.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_czgowi?.base??"",a=globalThis.__sveltekit_czgowi?.assets??s??"";export{a,s as b};
