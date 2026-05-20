e6354454ac22910ee04a5cae659915b2db7772f9
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-05-19 23:30:23 -0500

    Minor ui fix

--- _app/immutable/chunks/C8NBpzpO.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_h16y9r?.base??"",a=globalThis.__sveltekit_h16y9r?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/CAkuGcT5.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1d9e7ti?.base??"",e=globalThis.__sveltekit_1d9e7ti?.assets??s??"";export{e as a,s as b};
