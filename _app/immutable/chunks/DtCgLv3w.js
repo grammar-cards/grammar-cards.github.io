bea0dc2b9adb6ba18f4173b6fe1337dad6f5f8c2
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-08-23 22:05:51 -0500

    Subjunctive: spread triggers, drop wish from glossary

---
+++ _app/immutable/chunks/DtCgLv3w.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_f5f7fc?.base??"",a=globalThis.__sveltekit_f5f7fc?.assets??s??"";export{a,s as b};
--- _app/immutable/entry/start.CPfTnfO4.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/BwwoossP.js";export{o as load_css,r as start};
