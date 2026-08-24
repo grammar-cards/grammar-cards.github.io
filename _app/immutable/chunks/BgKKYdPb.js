d44b6c8c51b5f1d6fd6311e257bde2310c9ffb4e
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-08-23 21:10:55 -0500

    Clear parentheses from roles and corpus

---
+++ _app/immutable/chunks/BgKKYdPb.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_7pxcuq?.base??"",a=globalThis.__sveltekit_7pxcuq?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/Cz3735xF.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1k2wxg0?.base??"",a=globalThis.__sveltekit_1k2wxg0?.assets??s??"";export{a,s as b};
