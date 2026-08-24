61f28d119b296a7913637d38f116322b639c9b56
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-08-23 23:06:45 -0500

    Reported speech: structural variants, 32 reporting verbs, verified word choice

--- _app/immutable/chunks/DtCgLv3w.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_f5f7fc?.base??"",a=globalThis.__sveltekit_f5f7fc?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/RVNWvH5O.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_rlzamy?.base??"",a=globalThis.__sveltekit_rlzamy?.assets??s??"";export{a,s as b};
