3127850305389acbddbdc150d9298fc2446a6b3a
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-08-30 03:04:37 -0500

    Give every card a gloss so the long-press hint works

--- _app/immutable/chunks/BNDWZKaT.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_s6lk1j?.base??"",a=globalThis.__sveltekit_s6lk1j?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/Cs1AopKK.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_2az7ff?.base??"",a=globalThis.__sveltekit_2az7ff?.assets??s??"";export{a,s as b};
