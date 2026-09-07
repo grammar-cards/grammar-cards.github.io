a66372566028cdc6bed8c9986c07a01ed2bdeddf
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-09-07 14:44:31 -0500

    Fix weather sentence quality across 22 files

--- _app/immutable/chunks/B5vBN36A.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_101ljb0?.base??"",a=globalThis.__sveltekit_101ljb0?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/Bk0d-Pf-.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_ey8riv?.base??"",e=globalThis.__sveltekit_ey8riv?.assets??s??"";export{e as a,s as b};
