ba6ba64215deaed3be15b0717f6c1cfbef9fd68e
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-09-07 12:30:18 -0500

    Fix technology sentence quality across 22 issue groups

---
+++ _app/immutable/chunks/CZJW19Aa.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1lgvttf?.base??"",t=globalThis.__sveltekit_1lgvttf?.assets??s??"";export{t as a,s as b};
--- _app/immutable/chunks/D9SqO47M.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_13z9786?.base??"",a=globalThis.__sveltekit_13z9786?.assets??s??"";export{a,s as b};
