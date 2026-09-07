5ca8874e42cb71102692b24a2da7b3f06a62e960
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-09-07 14:20:48 -0500

    Fix hobbies sentence quality across 37 files

---
+++ _app/immutable/chunks/B5vBN36A.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_101ljb0?.base??"",a=globalThis.__sveltekit_101ljb0?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/CZJW19Aa.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1lgvttf?.base??"",t=globalThis.__sveltekit_1lgvttf?.assets??s??"";export{t as a,s as b};
