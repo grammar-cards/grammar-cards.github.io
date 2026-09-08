0fe072682ea1fe66c83aef6bc8601d38ced0cc80
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-09-07 19:41:43 -0500

    Fix software-engineering sentence quality across 53 files

---
+++ _app/immutable/chunks/4JPXSd8o.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_zsx1a6?.base??"",a=globalThis.__sveltekit_zsx1a6?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/DkVedOag.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1tpejn6?.base??"",e=globalThis.__sveltekit_1tpejn6?.assets??s??"";export{e as a,s as b};
