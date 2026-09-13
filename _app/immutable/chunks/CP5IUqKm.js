b8b1e38d701145e4435548ea56fc20ecf4b37755
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-09-13 15:46:51 -0500

    Align glossary entries with the structures the exercises use

--- _app/immutable/chunks/CP5IUqKm.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_gubr6f?.base??"",a=globalThis.__sveltekit_gubr6f?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/mdcUrDJh.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_z9brgf?.base??"",a=globalThis.__sveltekit_z9brgf?.assets??s??"";export{a,s as b};
