3cc0d2e14b553917c21a6f561e5e8311e8f16736
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-09-13 10:56:55 -0500

    Fix glossary text overflow in subjunctive categories

---
+++ _app/immutable/chunks/CP5IUqKm.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_gubr6f?.base??"",a=globalThis.__sveltekit_gubr6f?.assets??s??"";export{a,s as b};
--- _app/immutable/chunks/DeBO1kfX.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1vwz0l1?.base??"",a=globalThis.__sveltekit_1vwz0l1?.assets??s??"";export{a,s as b};
