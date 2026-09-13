7137fec00bfc2ffc14e7dd26ad98acf52ed0e4d5
Diego Dominguez <diegofernandodominguez@gmail.com>
2026-09-13 10:39:12 -0500

    Document the past subjunctive (were) in the glossary

--- _app/immutable/chunks/D5VIJifU.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_11a0mdx?.base??"",a=globalThis.__sveltekit_11a0mdx?.assets??s??"";export{a,s as b};
---
+++ _app/immutable/chunks/DeBO1kfX.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1vwz0l1?.base??"",a=globalThis.__sveltekit_1vwz0l1?.assets??s??"";export{a,s as b};
